5094e0844ca03fcd63079bc64b9c49b556d7c812
MarvanT <marvan.t98@gmail.com>
2023-11-02 16:15:53 +0000

    Added local SSL certificates for development
    Added local SSL certificates (localhost+1-key.pem and localhost+1.pem) to run the Vue.js dev server over HTTPS.Updated vue.config.js to read these certificates, which are necessary for secure communication during local development. The switch to HTTPS protocol is crucial for anticipated features that require a secure context.

--- book-recommendations/frontend/vue.config.js
+++ book-recommendations/frontend/vue.config.js
@@ -1,7 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs');
+
 module.exports = {
   devServer: {
     port: 3000,
+    https: {
+      key: fs.readFileSync('./local-certs/localhost+1-key.pem'),
+      cert: fs.readFileSync('./local-certs/localhost+1.pem'),
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
