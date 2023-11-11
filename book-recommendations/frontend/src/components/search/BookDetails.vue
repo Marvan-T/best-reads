fa5d4dcf49f400e2eeb3e6f3d11d1a72de41d63f
Marvan-T <65969444+Marvan-T@users.noreply.github.com>
2023-11-11 20:15:38 +0000

    Search result item ui updated

--- book-recommendations/frontend/src/components/search/BookDetails.vue
+++ book-recommendations/frontend/src/components/search/BookDetails.vue
@@ -1,27 +1,57 @@
 <template>
   <v-hover v-slot="{ hover }">
     <v-col
-      class="mt-12"
+      class="mt-4"
       cols="12"
+      md="6"
     >
-      <v-row>
-        <v-card
-          class="full-width-card"
-          :color="getHoverEffect(hover)"
-          outlined
-          flat
-          @click="emitViewBook"
-        >
-          <v-col cols="2">
-            <p>Some text</p>
-          </v-col>
+      <v-card
+        class="full-width-card"
+        :color="getHoverEffect(hover)"
+        outlined
+        flat
+        @click="emitViewBook"
+      >
+        <v-row>
           <v-col
-            cols="6"
+            cols="3"
+            class="d-flex align-center justify-center image-container"
           >
-            <p>Text 2 </p>
+            <v-img
+              :src="thumbnail"
+              class="thumbnail-size"
+              contain
+            />
           </v-col>
-        </v-card>
-      </v-row>
+          <v-col cols="9">
+            <v-card-text>
+              <div class="text-h6 mb-2">
+                {{ title }}
+              </div>
+              <div class="text-subtitle-1 grey--text mb-2">
+                {{ authors }}
+              </div>
+              <div class="text-body-2">
+                {{ getTruncatedDescription }}
+              </div>
+              <div class="mt-3">
+                <v-chip
+                  v-for="(category, index) in categories"
+                  :key="category"
+                  :class="{
+                    'ml-2': index > 0 && $vuetify.breakpoint.smAndUp,
+                    'mt-2': index > 0 && $vuetify.breakpoint.xsOnly
+                  }"
+                  outlined
+                  color="primary"
+                >
+                  {{ category }}
+                </v-chip>
+              </div>
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-col>
   </v-hover>
 </template>
@@ -52,6 +82,11 @@ export default {
       type: String,
       required: true
     },
+    categories: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
     origin: {
       type: String, // from which page the book is being viewed (effect how the book data is displayed on view book)
       required: true // either 'search' or 'other'
@@ -78,7 +113,7 @@ export default {
         : this.authorsWithFormattedDate();
     },
     getTruncatedDescription() {
-      const maxCharacterCount = 100;
+      const maxCharacterCount = 200;
       if (this.bookData.description.length > maxCharacterCount) {
         return this.bookData.description.substring(0, maxCharacterCount) + '...'
       }
@@ -131,7 +166,7 @@ export default {
       }
     },
     getHoverEffect(hover) {
-      return hover ? "blue-grey lighten-4" : "#FFFFFF";
+      return hover ? "blue-grey lighten-4" : "#D6D6D6";
     }
   }
 }
@@ -140,8 +175,17 @@ export default {
 <style scoped>
 
 .full-width-card {
-  width: 100%;
+  width: 99%;
+  height: 100%;
 }
 
+.image-container {
+  max-width: 200px;
+  max-height: 350px;
+}
 
+.thumbnail-size {
+  max-width: 80%;
+  max-height: 80%;
+}
 </style>
