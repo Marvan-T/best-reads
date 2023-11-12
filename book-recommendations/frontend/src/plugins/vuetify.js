beee0bed661adf08cd4772399c5a5ea5aaaba255
Marvan-T <65969444+Marvan-T@users.noreply.github.com>
2023-11-12 19:15:51 +0000

    Primary, secondary colours updated

--- book-recommendations/frontend/src/plugins/vuetify.js
+++ book-recommendations/frontend/src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
-})
+  theme: {
+    themes: {
+      light: {
+        primary: '#5D8263', //earthy green primary color
+        secondary: '#AD5D5D', //muted rose secondary color
+      },
+    },
+  },
+});
