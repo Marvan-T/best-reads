82ea734cd2a3b38756c2d7617f1ace24526e9705
MarvanT <marvan.t98@gmail.com>
2024-03-10 20:53:38 +0000

    temp fix for azure container app connection

--- book-recommendations/frontend/src/api/recommendations.js
+++ book-recommendations/frontend/src/api/recommendations.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export async function getRecommendations(book) {
-  const result = await axios.post("/api/extensions/BookRecommendations", book, {
+  const recommendationsUrl = "https://best-reads-extensionsv1.live/api/extensions/BookRecommendations"
+  const result = await axios.post(recommendationsUrl, book, {
     headers: {
       'Content-Type': 'application/json'
     }
